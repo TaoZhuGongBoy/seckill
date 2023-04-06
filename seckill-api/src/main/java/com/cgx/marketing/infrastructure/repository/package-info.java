c899b2d8a7fab51d13ebd27a0e2b1a9372a2ef46
taozhugongBoy <zhangfengshi@startdt.com>
2023-04-06 08:42:47 +0800

    关于DDD分层的注释说明

---
+++ seckill-api/src/main/java/com/cgx/marketing/infrastructure/repository/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author zhangfengshi
+ */
+package com.cgx.marketing.infrastructure.repository;
+
+/**
+ *repository:它存放所在聚合的查询或持久化领域对象的代码，通常包括仓储接口和仓储实现方法。为了方便聚合的拆分和组合，我们设定了一个原则：<b>一个聚合对应一个仓储</b><br>
+ */
---
+++ seckill-api/src/main/java/com/cgx/marketing/interfaces/activity/package-info.java
@@ -0,0 +1,6 @@
+package com.cgx.marketing.interfaces.activity;
+
+/**
+ * 用户界面层<br>
+ * 负责向用户展示信息和解释用户命令。这里的用户是广义概念，既可以是用户界面的使用者，还可以是与当前系统交互的其他应用。
+ */
